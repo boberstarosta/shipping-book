c85270531efd8c1d58d35b9d6a56c5d25424ccee
boberstarosta <boberstarosta@gmail.com>
2018-11-26 15:26:42 +0100

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+
+from app.gui import MainWindow
+
+
+app = MainWindow()
+
---
+++ app/gui.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.minsize(800, 800)
+
